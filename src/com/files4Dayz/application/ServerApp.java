4b79261d3bf13f6c66734cd112fe94d4e72d9793
alec3243 <alec3243@gmail.com>
2018-02-09 12:32:43 -0800

    MORE INITIAL FILES

---
+++ src/com/files4Dayz/application/ServerApp.java
@@ -0,0 +1,13 @@
+package com.files4Dayz.application;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class ServerApp extends Application {
+
+	@Override
+	public void start(Stage arg0) throws Exception {
+		
+	}
+
+}
