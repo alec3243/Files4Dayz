f2eab62b93d8355f2e18ad3c992ee241200b6245
Alec Strickland <alecjstrickland@gmail.com>
2018-02-11 20:52:28 -0800

    Client UI. Improved launcher UI

---
+++ src/com/files4Dayz/application/LauncherController.java
@@ -0,0 +1,151 @@
+package com.files4Dayz.application;
+
+import com.files4Dayz.server.Server;
+import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+import static com.files4Dayz.application.LauncherController.Selection.*;
+
+public class LauncherController extends Application {
+
+	@FXML
+	private Button startButton;
+
+	@FXML
+	private RadioButton serverButton;
+
+	@FXML
+	private RadioButton clientButton;
+
+	@FXML
+	private TextField ip;
+	@FXML
+	private TextField port;
+
+	protected enum Selection {
+		CLIENT,SERVER;
+	}
+
+	public static void main(String[] args) {
+		launch();
+	}
+
+	@Override
+	public void start(final Stage primaryStage) throws Exception {
+		final FXMLLoader loader = new FXMLLoader(getClass().getResource("LauncherApp.fxml"));
+		final Parent root = loader.load();
+		final Scene scene = new Scene(root);
+		primaryStage.setScene(scene);
+		primaryStage.show();
+
+		//Establish nodes
+		startButton = (Button) root.lookup("#startButton");
+		startButton.setDisable(true);
+		serverButton = (RadioButton) root.lookup("#serverButton");
+		clientButton = (RadioButton) root.lookup("#clientButton");
+		final ToggleGroup toggleGroup = new ToggleGroup();
+		serverButton.setToggleGroup(toggleGroup);
+		clientButton.setToggleGroup(toggleGroup);
+
+		// Get radio button selection
+		final Selection[] selection = new Selection[1];
+		toggleGroup.selectedToggleProperty().addListener(new ChangeListener<Toggle>() {
+			@Override
+			public void changed(ObservableValue<? extends Toggle> observable, Toggle oldValue, Toggle newValue) {
+				if (((RadioButton)newValue.getToggleGroup().getSelectedToggle()).getText().equals("Client")) {
+					selection[0] = CLIENT;
+					if (!ipEmpty() && !portEmpty()) {
+						startButton.setDisable(false);
+					} else {
+						startButton.setDisable(true);
+					}
+				} else {
+					selection[0] = SERVER;
+					if (!portEmpty()) {
+						startButton.setDisable(false);
+					} else {
+						startButton.setDisable(true);
+					}
+				}
+			}
+		});
+
+		ip = (TextField) root.lookup("#ip");
+		port = (TextField) root.lookup("#port");
+		// Set listener for data change. args aren't used
+		ip.textProperty().addListener((arg1,arg2,arg3) -> {
+			if (selection[0] == SERVER) {
+				if (portEmpty()) {
+					startButton.setDisable(true);
+				} else {
+					startButton.setDisable(false);
+				}
+			} else {
+				if (!ipEmpty() && !portEmpty()) {
+					startButton.setDisable(false);
+				} else {
+					startButton.setDisable(true);
+				}
+			}
+			if (toggleGroup.getSelectedToggle() == null) {
+				startButton.setDisable(true);
+			}
+		});
+		port.textProperty().addListener((arg1,arg2,arg3) -> {
+			if (portEmpty()) {
+				startButton.setDisable(true);
+			} else if (!ipEmpty() && !portEmpty()) {
+				startButton.setDisable(false);
+			}
+			if (toggleGroup.getSelectedToggle() == null) {
+				startButton.setDisable(true);
+			}
+		});
+
+		// Establish start button functionality
+		startButton.setOnAction((event) -> {
+
+			System.out.println("Selected: " + selection[0]);
+			System.out.println(ip.getText() + ":" + port.getText());
+			try {
+				nextApplication(ip.getText(),port.getText(), selection[0],primaryStage);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		});
+	}
+
+	private boolean portEmpty() {
+		return port.getText().isEmpty();
+	}
+
+	private boolean ipEmpty() {
+		return ip.getText().isEmpty();
+	}
+
+	private void nextApplication(String ip, String port, Selection selection,Stage currentStage) throws IOException {
+		Stage stage = new Stage();
+		currentStage.close();
+		switch(selection) {
+			case CLIENT:
+				ClientController clientApp = new ClientController();
+				clientApp.start(currentStage, ip, Integer.parseInt(port));
+				break;
+			case SERVER:
+				ServerController serverApp = new ServerController();
+				serverApp.start(currentStage,Integer.parseInt(port));
+				break;
+		}
+	}
+}
--- src/com/files4Dayz/application/ServerController.java
+++ src/com/files4Dayz/application/ServerController.java
@@ -3,11 +3,15 @@ package com.files4Dayz.application;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
-public class ServerApp extends Application {
+public class ServerController extends Application {
 
 	@Override
 	public void start(Stage arg0) throws Exception {
-		
+		start(arg0,0);
+	}
+
+	public void start(Stage primaryStage, int port) {
+
 	}
 
 }
