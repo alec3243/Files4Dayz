892ea8137a141ceee08f4076669e12fdd8f5c8d1
Zhou Hong <dylanhong325@gmail.com>
2018-02-18 13:00:03 -0800

    add decode method and server side send method, login info

--- src/com/files4Dayz/server/Server.java
+++ src/com/files4Dayz/server/Server.java
@@ -5,19 +5,41 @@ import java.io.*;
 public class Server {
 	private ServerSocket server;
 	private Socket client;
-	public void runServer(int port) throws IOException{
-		server = new ServerSocket(port);
-		if (server != null) {
+	private final String userName = "admin";
+	private final String passWord = "abc123";
+	
+	public Server(int port){
+		try {
+			server = new ServerSocket(port);
 			System.out.println("Server established. Waiting for client to send file");
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
-		client = server.accept();
-		saveFile(client);
+	}
+	
+	public void runServer() {
+		while (true) {
+			try {
+				client = server.accept();
+				saveFile(client);
+			} catch(IOException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 	private void saveFile(Socket client) throws IOException {
 		DataInputStream dataReadIn = new DataInputStream(client.getInputStream());
-		FileOutputStream file = new FileOutputStream("copy.jpg");
+		
+		// save as fileName sent from client
+		String fileName = dataReadIn.readUTF();
+		FileOutputStream file = new FileOutputStream(fileName);
+		
 		// set each reading chunk to be 1024
 		byte[] buffer = new byte[1024];
+		
+		// decode
+		byte[] data = decode(buffer);
+		
 		dataReadIn.read(buffer);
 		int read = 0;
 		int totalRead = 0;
@@ -28,5 +50,23 @@ public class Server {
 		System.out.println("File size is " + totalRead + " bytes");
 		dataReadIn.close();
 		file.close();
+	}
+	private void sendFile(String file) throws IOException {
+		DataOutputStream dos = new DataOutputStream(client.getOutputStream());
+		FileInputStream fileReadIn = new FileInputStream(file);
+		dos.writeUTF(file);
+		dos.flush();
+		byte[] buffer = new byte[1024];
+		
+		while (fileReadIn.read(buffer) > 0) {
+			dos.write(buffer);
+		}
+		
+		fileReadIn.close();
+		dos.close();	
+	}
+	private byte[] decode(byte[] buffer) {
+		// TODO: Decode hashed data
+		return new byte[] {};
 	}
 }
--- src/com/files4Dayz/stuff/Main.java
+++ src/com/files4Dayz/stuff/Main.java
@@ -4,8 +4,7 @@ import com.files4Dayz.server.Server;
 public class Main {
 
 	public static void main(String[] args) {
-		Server server = new Server();
-		
+		Server s = new Server(1343);
+		s.runServer();
 	}
-	
 }
